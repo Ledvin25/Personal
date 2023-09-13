17965055eee43be04f39a0e78ff5c8a04a7b5018
Ledvin Manuel Leiva Mata <Ledvin25@hotmail.com>
2023-09-13 11:34:25 -0600

    Delete TEC/.history/Taller directory

--- TEC/.history/Taller/Tareas/tarea6_Ledvin_Manuel_Leiva_Mata_20230506140205.py
+++
@@ -1,4 +0,0 @@
-#Tarea 6 - Ledvin Manuel Leiva Mata
-
-# para el siguiente codigo se utilizara habitualmente i, j y k como contadores por la convencion ya existente.
-
--- TEC/.history/Taller/Tareas/tarea6_Ledvin_Manuel_Leiva_Mata_20230506180615.py
+++
@@ -1,8 +0,0 @@
-#Tarea 6 - Ledvin Manuel Leiva Mata
-
-# para el siguiente codigo se utilizara habitualmente i, j y k como contadores por la convencion ya existente.
-
-#Ejercicio 1
-
-def lugares_de_votacion():
-    
