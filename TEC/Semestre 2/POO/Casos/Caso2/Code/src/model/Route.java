7e301a21c4955d66b2cbbca87667ec110b39f12c
Ledvin25 <led_25.ok@hotmail.es>
2023-09-30 22:09:58 -0600

    add route & stop

---
+++ TEC/Semestre 2/POO/Casos/Caso2/Code/src/model/Route.java
@@ -0,0 +1,30 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Route {
+
+    private List<Stop> stops;
+
+    public Route() {
+        this.stops = new ArrayList<>();
+    }
+
+    public void addStop(Stop stop) {
+        stops.add(stop);
+    }
+
+    public void removeStop(Stop stop) {
+        stops.remove(stop);
+    }
+
+    // Método para calcular la distancia total de la ruta
+    public double calculateTotalDistance() {
+        double totalDistance = 0.0;
+        for (int i = 0; i < stops.size() - 1; i++) {
+            totalDistance += stops.get(i).getDistance();
+        }
+        return totalDistance;
+    }
+}
---
+++ TEC/Semestre 2/POO/Casos/Caso2/Code/src/model/Stop.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Stop 
+{
+    private String name;
+    private double distance; // Distancia a la siguiente parada en la ruta
+    private boolean terminal; // Indica si la parada es terminal o no
+
+    public Stop(String name, double distance, boolean terminal) {
+        this.name = name;
+        this.distance = distance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    // Método para comprobar si la parada es terminal o no
+    public boolean isTerminal() {
+        return terminal;
+    }
+    
+}
